1ed710ab495d196824303bbdb90ba044426bfa89
marcosmercuri <marcosmercuri@a6ee79f5-99a2-0c8b-c91d-5a428200af1e>
2012-03-06 00:57:20 +0000

    Agregue un par de metodos para multiplicar una sola imagen por W. Y en el MainTest agregue un ejemplo de prueba calculando la distancia.

--- code/src/img/main/MainTest.java
+++ code/src/img/main/MainTest.java
@@ -2,8 +2,12 @@ package img.main;
 
 import img.dataobjects.PCA;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import Jama.Matrix;
 
 
 public class MainTest {
@@ -19,14 +23,59 @@ public class MainTest {
 			logger.info("-------- INICIO ENTRENAMIENTO --------");
 			pca.entrenar();
 			logger.info("--------  FIN ENTRENAMIENTO --------");
+			
 			logger.info("-------- INICIO REFERENCIA --------");
 			pca.prepararMatrizXReferencia();
 			logger.info("--------  FIN REFERENCIA --------");
+			
+			logger.info("--------  INICIO OBTENCION DE Y --------");
+			pca.proyectarImagenesDeReferencia();
+			logger.info("--------  FIN OBTENCION DE Y --------");
+			
+			List<int[]> imagenes = new ArrayList<int[]>();
+			pca.leer("C:\\teocom\\test\\", imagenes);
+			Matrix imagenesMatrix = pca.pasarAEigenface(imagenes);
+			
+			double[] imagenTest = MainTest.matrizToArray(0, imagenesMatrix);
+			double distanciaMenor = 99999;
+			int indexMenor = 0;
+			double aux;
+
+			for (int i = 0; i < pca.getY().getColumnDimension(); i++) {
+				double[] imagenMatriz = MainTest.matrizToArray(i, pca.getY());
+				aux = MainTest.distancia(imagenMatriz, imagenTest);
+				if (aux < distanciaMenor) {
+					distanciaMenor = aux;
+					indexMenor = i;
+				}
+			}
+			
+			System.out.println(indexMenor);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			logger.log(Level.SEVERE, "Error critico, no puede ejecutarse la aplicacion." + e.getCause());
 			e.printStackTrace();
 		}
+		
+	}
+	
+	private static double[] matrizToArray (int index, Matrix matriz) {
+		double[] imagenTest = new double[matriz.getRowDimension()];
+		for (int i = 0; i < matriz.getRowDimension(); i++) {
+			imagenTest[i] = matriz.get(i, index);
+		}
+		return imagenTest;
+	}
+	
+	private static double distancia(double[] array1, double[] array2) {
+		double distancia;
+		double suma = 0;
+		for (int i = 0; i < array1.length; i++) {
+			suma = suma + ( (array1[i] - array2[i]) * (array1[i] - array2[i]) );
+		}
+		distancia = Math.sqrt(suma);
+		return distancia;
 	}
 		
 
